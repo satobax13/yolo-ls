3d3358cb478c76ffba7e44b97dc330db03d5275e
satobax13 <pazh91@mail.ru>
2025-08-13 15:25:42 +0300

    Готовый exmaple_run

--- yolo_auto_label/example_run.py
+++ yolo_auto_label/example_run.py
@@ -1,30 +1,24 @@
 from YoloClassAutolabel import YoloAutoLabel
 from pathlib import Path
+from prefect import flow
 import yaml
 
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+@flow
+def yolo_autolabel_flow():
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-API_KEY = config["api_key"]
-MODEL_NAME = config["model_name"]
-PROJECT_NAME = config["project_name"]
-UPLOAD_AS_ANNOTATIONS = config["upload_as_annotations"]
-HOST_LOCAL_STORAGE = config["host_local_storage_path"]
-LABEL_STUDIO_URL = "http://label-studio:8080"
-
-
-
-def main():
-    # Инициализация
     auto_label = YoloAutoLabel(
-        label_studio_url=LABEL_STUDIO_URL,
-        ls_api_key=API_KEY,
-        yolo_model_path= str(Path(__file__).parent.parent / "models" / MODEL_NAME),
-        host_local_storage_path=HOST_LOCAL_STORAGE
+        label_studio_url="http://label-studio:8080",
+        ls_api_key=config["api_key"],
+        yolo_model_path=str(Path(__file__).parent.parent / "models" / config["model_name"]),
+        host_local_storage_path=config["host_local_storage_path"]
     )
-    # Запуск обработки
-    auto_label.run(project_name=PROJECT_NAME, upload_as_annotations=UPLOAD_AS_ANNOTATIONS)
+    auto_label.run(
+        project_name=config["project_name"],
+        upload_as_annotations=config["upload_as_annotations"]
+    )
 
 
 if __name__ == "__main__":
-    main()
+    yolo_autolabel_flow()
